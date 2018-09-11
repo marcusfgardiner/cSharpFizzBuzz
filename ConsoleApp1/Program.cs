b9688deea231216ec7204e91c99706f704771214
Marcus Gardiner <marcus.gardiner@softwire.com>
2018-09-11 08:56:47 +0100

    First fizz and buzz

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public class FizzBuzzer
+     {
+        public void FizzBuzz(int number)
+        {
+            string fizzBuzzOutput = "Fizz";
+            if (number == 3)
+            {
+                fizzBuzzOutput = "Fizz";
+            }
+            else
+            {
+                fizzBuzzOutput = "Buzz";
+            }
+            System.Console.WriteLine(fizzBuzzOutput);
+        }
+     }
+    class TestFizzBuzz
+    {
+        static void Main()
+        {
+            var fizzbuzzer = new FizzBuzzer();
+            fizzbuzzer.FizzBuzz(3);
+            fizzbuzzer.FizzBuzz(5);
+            Console.ReadKey();
+        }
+    }
+}
